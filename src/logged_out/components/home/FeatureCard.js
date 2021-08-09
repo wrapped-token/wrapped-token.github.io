910f6ad55d41da9b35ce4a43e6eacf867b78cd76
spvyerra <spvyerra@umich.edu>
2021-08-08 20:33:30 -0400

    Layout for Feature Card created

--- src/logged_out/components/home/FeatureCard.js
+++ src/logged_out/components/home/FeatureCard.js
@@ -1,63 +1,47 @@
-import React, { Fragment } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import { Typography, withStyles } from "@material-ui/core";
+import { Grid, Typography, withStyles, isWidthUp } from "@material-ui/core";
 
 const styles = theme => ({
-  iconWrapper: {
-    borderRadius: theme.shape.borderRadius,
-    textAlign: "center",
-    display: "inline-flex",
-    alignItems: "center",
+  icon: {
+
+  },
+  content: {
+    display: "flex",
+    "flex-direction": "column",
     justifyContent: "center",
     marginBottom: theme.spacing(3),
     padding: theme.spacing(1) * 1.5
   }
 });
 
-function shadeColor(hex, percent) {
-  const f = parseInt(hex.slice(1), 16);
-
-  const t = percent < 0 ? 0 : 255;
-
-  const p = percent < 0 ? percent * -1 : percent;
-
-  const R = f >> 16;
-
-  const G = (f >> 8) & 0x00ff;
-
-  const B = f & 0x0000ff;
-  return `#${(
-    0x1000000 +
-    (Math.round((t - R) * p) + R) * 0x10000 +
-    (Math.round((t - G) * p) + G) * 0x100 +
-    (Math.round((t - B) * p) + B)
-  )
-    .toString(16)
-    .slice(1)}`;
-}
-
 function FeatureCard(props) {
-  const { classes, Icon, color, headline, text } = props;
+  const { classes, Icon, width, headline, text, mdDelay, smDelay } = props;
   return (
-    <Fragment>
-      <div
-        // We will set color and fill here, due to some prios complications
-        className={classes.iconWrapper}
-        style={{
-          color: color,
-          backgroundColor: shadeColor(color, 0.5),
-          fill: color
-        }}
-      >
+    <Grid container
+      spacing={24}
+      data-aos="zoom-in-up"
+      data-aos-delay={
+        isWidthUp("md", width) ? mdDelay : smDelay
+      }>
+      <Grid
+        item
+        className={classes.content}
+        xs={6}>
+        <Typography variant="h3" paragraph>
+          {headline}
+        </Typography>
+        <Typography variant="body1" color="textSecondary">
+          {text}
+        </Typography>
+      </Grid>
+      <Grid
+        item
+        className={classes.icon}
+        xs={6}>
         {Icon}
-      </div>
-      <Typography variant="h5" paragraph>
-        {headline}
-      </Typography>
-      <Typography variant="body1" color="textSecondary">
-        {text}
-      </Typography>
-    </Fragment>
+      </Grid>
+    </Grid >
   );
 }
 
